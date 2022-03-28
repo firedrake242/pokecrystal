3c6a2f6545b840ee836b6d6f7d1467d5a2f4e890
firedrake242 <firedrakerudy@gmail.com>
2022-03-28 12:34:07 -0400

    tips from the discord
    I removed the commented out 'handledefrost' subroutine that I had removed earlier, bc git does handle that for me lol

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -1521,64 +1521,7 @@ HandleFutureSight:
 	call UpdateBattleMonInParty
 	jp UpdateEnemyMonInParty
 
-; HandleDefrost:
-	; ldh a, [hSerialConnectionStatus]
-	; cp USING_EXTERNAL_CLOCK
-	; jr z, .enemy_first
-	; call .do_player_turn
-	; jr .do_enemy_turn
 
-; .enemy_first
-	; call .do_enemy_turn
-; .do_player_turn
-	; ld a, [wBattleMonStatus]
-	; bit FRZ, a
-	; ret z
-
-	; ld a, [wPlayerJustGotFrozen]
-	; and a
-	; ret nz
-
-	; call BattleRandom
-	; cp 10 percent
-	; ret nc
-	; xor a
-	; ld [wBattleMonStatus], a
-	; ld a, [wCurBattleMon]
-	; ld hl, wPartyMon1Status
-	; call GetPartyLocation
-	; ld [hl], 0
-	; call UpdateBattleHuds
-	; call SetEnemyTurn
-	; ld hl, DefrostedOpponentText
-	; jp StdBattleTextbox
-
-; .do_enemy_turn
-	; ld a, [wEnemyMonStatus]
-	; bit FRZ, a
-	; ret z
-	; ld a, [wEnemyJustGotFrozen]
-	; and a
-	; ret nz
-	; call BattleRandom
-	; cp 10 percent
-	; ret nc
-	; xor a
-	; ld [wEnemyMonStatus], a
-
-	; ld a, [wBattleMode]
-	; dec a
-	; jr z, .wild
-	; ld a, [wCurOTMon]
-	; ld hl, wOTPartyMon1Status
-	; call GetPartyLocation
-	; ld [hl], 0
-; .wild
-
-	; call UpdateBattleHuds
-	; call SetPlayerTurn
-	; ld hl, DefrostedOpponentText
-	; jp StdBattleTextbox
 
 HandleSafeguard:
 	ldh a, [hSerialConnectionStatus]
@@ -6629,7 +6572,7 @@ ApplyPrzEffectOnSpeed:
 .enemy_ok
 	ld [hl], b
 	ret
-	
+
 ApplyFrzEffectOnSpclAtk:
 	ldh a, [hBattleTurn]
 	and a
@@ -6647,11 +6590,11 @@ ApplyFrzEffectOnSpclAtk:
 	or b
 	jr nz, .player_ok
 	ld b, $1 ; min special attack
-	
+
 	.player_ok
 	ld [hl], b
 	ret
-	
+
 	.enemy
 	ld a, [wEnemyMonStatus]
 	and 1 << BRN
@@ -6670,7 +6613,7 @@ ApplyFrzEffectOnSpclAtk:
 .enemy_ok
 	ld [hl], b
 	ret
-	
+
 ApplyBrnEffectOnAttack:
 	ldh a, [hBattleTurn]
 	and a
